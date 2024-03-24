afc030002def78ca7b4ba05684e67eab64bbe003
Pedro Palma <pedro.palma@uc.cl>
2024-03-24 14:53:07 -0300

    feat: Add htmlWritter function

---
+++ src/htmlWriter.js
@@ -0,0 +1,27 @@
+const { writeFile } = require('fs');
+
+const htmlWriter = (filename, content) => {
+  const formattedContent = content.replace(/\n/g, '\n    ').trim();
+  const html = 
+  `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>${filename}</title>
+    </head>
+    <body>
+      ${formattedContent}
+    </body>
+  </html>
+  `.trim().replace(/^\s\s/gm, '');
+
+  writeFile(`${filename}.html`, html, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`${filename}.html written successfully!`);
+  });
+}
+
+module.exports = htmlWriter;
